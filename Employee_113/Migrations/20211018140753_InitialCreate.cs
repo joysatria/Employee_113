6e8f0a5290191eecf8e8c152234571450ba5648e
Muhammad <muhammadfajarisa@gmail.com>
2021-10-18 21:32:35 +0700

    Add project files.

---
+++ Employee_113/Migrations/20211018140753_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Employee_113.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    EmpCode = table.Column<string>(type: "varchar(10)", nullable: true),
+                    Position = table.Column<string>(type: "varchar(100)", nullable: true),
+                    OfficeLocation = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
